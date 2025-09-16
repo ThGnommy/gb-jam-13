85c80edc85230948fd36d476e976e2aecd28984e
Kape99 <lucapecchi13@gmail.com>
2025-09-16 19:04:23 +0200

    add comments

--- scenes/scripts/enemy2xspeed.gd
+++ scenes/scripts/enemy2xspeed.gd
@@ -1,17 +1,13 @@
-
-
-
 extends Enemy
 
 #test class to see if we can change behaviour of the enemies
 
-func _ready() -> void:
-	position = position.snapped(Vector2.ONE * TILE_SIZE)
-	position += Vector2.ONE * TILE_SIZE / 2
-
 func do_action(target):
+	# this method can be used to customie the behaviour of the enemies, it can attack/move/prepare/wait in response to the player position
 	var dir = _choose_direction(target)
 	var tween = create_tween()
+
+	# here we move twice in the same direction
 	var direction = position + dir * TILE_SIZE * 2
 	tween.tween_property(self, "position", direction, 1.0 / animation_speed).set_trans(Tween.TRANS_SINE)
 	moving = true
@@ -20,6 +16,3 @@ func do_action(target):
 	moving = false
 
 
-func die():
-	print("Object destroyed")
-	queue_free()
