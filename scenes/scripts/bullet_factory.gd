d1fed39948c9fd57075761ae99f00741f9aa7e85
Lauro-Vincenzo <vincenzo@zuru.com>
2025-09-22 11:26:33 +0200

    Fix mortar bullet marker bug

--- scenes/scripts/bullet_factory.gd
+++ scenes/scripts/bullet_factory.gd
@@ -50,6 +50,8 @@ func shoot_bullet(bullet_type: String, shooter_position: Vector2, shooter_direct
 
 func shoot_bullet_at_range(bullet_type: String, shooter_position: Vector2, shooter_direction: Vector2, range: int, shooter) -> void:
 	var bullet_instance : Bullet = create_bullet(bullet_type)
+	if (bullet_instance.has_method("disableMarker")):
+		bullet_instance.disableMarker()
 	bullet_instance.global_position = shooter_position + shooter_direction * (GridManager.CELL_SIZE * bullet_offset_mult(bullet_type))
 	bullet_instance.bullet_destroyed.connect(shooter._pass_turn)
 	shooter.get_parent().add_child(bullet_instance)
